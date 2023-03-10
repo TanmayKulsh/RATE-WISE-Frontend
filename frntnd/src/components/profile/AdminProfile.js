11a9d4eed4d11addbeb0e55295d7e603c9df4789
TanmayKulsh <tanmaykulsh@gmail.com>
2023-03-11 03:00:59 +0530

    adding view profile for adminj

--- frntnd/src/components/profile/AdminProfile.js
+++ frntnd/src/components/profile/AdminProfile.js
@@ -34,18 +34,18 @@ const AdminPage = () => {
    <br></br>
     </div>
     <h4>Artist Operations</h4>
-    <a href="/addartist" style={{color:"yellow"}}>Add Artist</a>
+    <a href="/addartist" style={{color:"yellow"}}>Add Artist</a><br></br>
     <a href="/deleteartist" style={{color:"yellow"}}>Delete Artist</a>
     <br></br>
     <br></br>
     <h4>Content Operations</h4>
-    <a href="/addcontent" style={{color:"yellow"}}>Add Content</a>
+    <a href="/addcontent" style={{color:"yellow"}}>Add Content</a><br></br>
     <a href="/deletecontent" style={{color:"yellow"}}>Delete Content</a>
     <br></br>
     <br></br>
     <h4>User Operations</h4>
-    <a href="/deleteuser" style={{color:"yellow"}}>Add Content</a>
-    <a href="/upgradeuser" style={{color:"yellow"}}>Add Content</a>
+    <a href="/deleteuser" style={{color:"yellow"}}>Delete User (deactivate Account)</a><br></br>
+    <a href="/upgradeuser" style={{color:"yellow"}}>Upgrade user to premium</a>
     
     </>
   );
